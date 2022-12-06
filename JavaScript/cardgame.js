094dcc4b6425ec54b1e3cdc1b5fde1f58afaeb15
Haninz <enghaneen_s@hotmail.com>
2022-12-06 13:09:51 +0200

    pokemon

---
+++ JavaScript/cardgame.js
@@ -0,0 +1,15 @@
+class Card {
+    constructor(name,cost){
+        this.name= name;
+        this.cost= cost;
+    }
+}
+
+
+class Unit extends Card {
+    constructor(name, cost,power, res){
+        super(name,cost);
+        this.power= power;
+        this.re= res;
+    }
+}
