6c921db7457c4e9370f28cd177899657495707d4
Haninz <enghaneen_s@hotmail.com>
2022-12-07 15:25:05 +0200

    putting it together

---
+++ React/putting-it-together/src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import BirthdayCard from './components/BirthdayCard';
+
+function App() {
+  return (
+    <div >
+      <BirthdayCard  firstname={"jane"}  lastname={"Doe"} age={40} hair={"brown"}/>
+    </div>
+  );
+}
+
+export default App;
---
+++ React/putting-it-together/src/components/BirthdayCard.jsx
@@ -0,0 +1,28 @@
+import React, {Component} from 'react';
+
+class BirthdayCard extends Component{
+    constructor(props){
+        super(props);
+        this.state={
+            counter:this.props.age,
+        };
+     
+    }
+    myfun =() => {
+        this.setState({counter:this.state.counter+1})
+    }
+
+
+    render(){
+        return (
+<div>
+    <h1>{this.props.firstname} {this.props.lastname}</h1>
+    <p>{this.state.counter}</p>
+    <p>{this.props.hair}</p>
+    <button  onClick={this.myfun} >Birthday Button</button>
+</div>
+        )
+    }
+}
+
+export default BirthdayCard;
