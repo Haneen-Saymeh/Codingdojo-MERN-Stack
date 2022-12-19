44c531561bd034b2eb04c04ad853e8e158f3d558
Haninz <enghaneen_s@hotmail.com>
2022-12-19 22:07:26 +0200

    product manager

--- Full-Stack-MERN/product-manager/client/src/components/ProductList.js
+++ Full-Stack-MERN/product-manager/client/src/components/ProductList.js
@@ -3,14 +3,25 @@ import axios from 'axios';
 import {Link} from '@reach/router';
     
 const ProductList = (props) => {
+
+    const deleteProduct = (productId) => {
+        axios.delete('http://localhost:8000/api/products/' + productId)
+            .then(res => console.log(res)
+                // removeFromDom(personId)
+            )
+            .catch(err => console.error(err));
+    }
     return (
         <div  style={{marginLeft:"39%", marginTop:"3%"}}>
-            <h>All products</h>
+            <h3>All products</h3>
             {props.products.map( (product, i) =>
             <div>
               <Link to={"/products/" + product._id} key={i}>
               {product.title}
           </Link>
+          <button onClick={(e)=>{deleteProduct(product._id)}}>
+                        Delete
+                    </button>
           </div>
           
             )}
--- Full-Stack-MERN/product-manager/client/src/views/Detail.js
+++ Full-Stack-MERN/product-manager/client/src/views/Detail.js
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import axios from 'axios';
+import {Link} from '@reach/router';
+import { navigate } from '@reach/router';
 
     
 const Detail = (props) => {
@@ -11,12 +13,27 @@ const Detail = (props) => {
             .then(res => setProduct(res.data))
             .catch(err => console.error(err));
     }, []);
+
+
+    const deleteProduct = (productId) => {
+        axios.delete('http://localhost:8000/api/products/' + productId)
+            .then(res => console.log(res), navigate('/products'))
+            .catch(err => console.error(err));
+    }
     
     return (
         <div>
             <h3>{product.title}</h3>
             <p>Price: {product.price}</p>
             <p>Description: {product.desc}</p>
+            <Link to={"/products/" + product._id + "/edit"}    >  Edit
+</Link>
+            <button onClick={(e)=>{deleteProduct(product._id)}} style={{marginLeft:"5px"}}>
+                        Delete
+                    </button>
+  
+
+
         </div>
     )
 }
