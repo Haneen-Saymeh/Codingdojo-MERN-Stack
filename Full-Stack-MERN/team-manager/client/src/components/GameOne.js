1a0b723ea21f5a0b4447655e1183b81260dbe635
Haninz <enghaneen_s@hotmail.com>
2022-12-25 00:59:35 +0200

    team manager baby steps

--- Full-Stack-MERN/team-manager/client/src/components/GameOne.js
+++ Full-Stack-MERN/team-manager/client/src/components/GameOne.js
@@ -5,8 +5,11 @@ const GameOne = () => {
 
 
         const [players, setPlayers] = useState([]);
+        const [player, setPlayer] = useState("");
         const [loaded, setLoaded] = useState(false);
-        const  [status, setStatus] = useState("undecided");
+        const  [game1, setGame1] = useState("");
+        const [name, setName] = useState("");
+    const [position, setPosition] = useState("");
     
         useEffect(()=>{
             axios.get('http://localhost:8000/api/players')
@@ -15,7 +18,47 @@ const GameOne = () => {
                     setLoaded(true);
                 })
                 .catch(err => console.error(err));
-        },[]);
+        },[players]);
+
+
+       
+
+
+      const handle1 = i => {
+        
+        axios.put('http://localhost:8000/api/players/' + i, {
+          
+           
+            game1: "playing",
+            
+        })
+            .then(res => console.log(res))
+            .catch(err => console.error(err));
+    }
+
+    const handle2 = i => {
+     
+      axios.put('http://localhost:8000/api/players/' + i, {
+        
+         
+          game1: "not playing",
+          
+      })
+          .then(res => console.log(res))
+          .catch(err => console.error(err));
+  }
+
+  const handle3 = i => {
+    
+    axios.put('http://localhost:8000/api/players/' + i, {
+      
+        
+        game1: "undecided",
+        
+    })
+        .then(res => console.log(res))
+        .catch(err => console.error(err));
+}
     
     
     
@@ -26,28 +69,34 @@ const GameOne = () => {
     <div>
 
 <table>
+  <thead>
   <tr>
     <th>name</th>
     
     <th>Actions</th>
     
   </tr>
+  </thead>
+  <tbody>
+  
 {
-   loaded && players.map((player,i)=> 
-  <tr key={i}>
+   loaded && players.map((player,idx)=> 
+  <tr key={idx}>
     <td>{player.name}</td>
     
     <td>
 
-      <button>Playing</button>
-      <button>Not playing</button>
-      <button>Undecided</button>
+      <button onClick={(e)=>handle1(player._id)} style={{backgroundColor:player.game1=="playing"?"green" :""}}>Playing</button>
+      <button onClick={(e)=>handle2(player._id)}  style={{backgroundColor:player.game1=="not playing"?"red" :""}}>Not playing</button>
+      <button onClick={(e)=>handle3(player._id)}  style={{backgroundColor:player.game1=="undecided"?"yellow" :""}}  >Undecided</button>
       
       
      </td>
    
   </tr>
 )}
+
+</tbody>
  
 </table>
       
